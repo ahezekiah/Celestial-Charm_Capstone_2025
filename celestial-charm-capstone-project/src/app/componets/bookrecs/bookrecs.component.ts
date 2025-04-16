26615d6088b7fe134f506229c32b0de12ec786f5
Amanda Hezekiah <amanda.hezekiah@gmail.com>
2025-04-16 11:13:20 -0600

    Foundation P1

---
+++ celestial-charm-capstone-project/src/app/componets/bookrecs/bookrecs.component.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-bookrecs',
+  imports: [],
+  templateUrl: './bookrecs.component.html',
+  styleUrl: './bookrecs.component.scss'
+})
+export class BookrecsComponent {
+
+}
