65214332a605ccaf6bb6bd486f3b2a9ad9b543c1
Amanda Hezekiah <amanda.hezekiah@gmail.com>
2025-05-02 22:06:15 -0600

    Adding  Main page for after login/register

--- celestial-charm-csp/src/App.jsx
+++ celestial-charm-csp/src/App.jsx
@@ -5,6 +5,7 @@ import Kpop from './components/Kpop/Kpop';
 import Anime from './components/Anime/Anime';
 import NotFound from './components/NotFound/NotFound';
 import Reviews from './components/Reviews/Reviews';
+import Main from './components/Main/Main';
 import { Route, Routes } from 'react-router-dom'; 
 
 function App() {
@@ -15,6 +16,8 @@ function App() {
       <Route path='/kpop' element={<Kpop />} />
       <Route path='/anime' element={<Anime />} />
       <Route path='/reviews' element={<Reviews />} />
+      <Route path='/main' element={<Main />} />
+      {/* <Route path='/reviews/:id' element={<Reviews />} /> */}
       {/* <Route path='/register' element={<Register />} />
       <Route path='/login' element={<Login />} /> */}
       {/* You can add more <Route> for other pages later */}
