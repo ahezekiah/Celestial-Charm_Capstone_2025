2b35945967e6c7c014f44baba44cc423070c5c60
Amanda Hezekiah <amanda.hezekiah@gmail.com>
2025-05-06 14:37:27 -0600

    Update Register.jsx

--- celestial-charm-csp/src/client/components/Register/Register.jsx
+++ celestial-charm-csp/src/client/components/Register/Register.jsx
@@ -18,6 +18,7 @@ export default function Register() {
 
     const handleChange = e => setForm({ ...form, [e.target.name]: e.target.value });
 
+
     const handleSubmit = async e => {
         e.preventDefault();
         try {
