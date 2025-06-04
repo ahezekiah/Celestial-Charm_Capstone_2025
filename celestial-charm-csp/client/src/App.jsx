0246abfbcd640de48fa7cb0a5f606ea6225d37a9
Amanda Hezekiah <amanda.hezekiah@gmail.com>
2025-06-04 04:21:13 -0600

    Page Links
    All of the pages are now here.

--- celestial-charm-csp/client/src/App.jsx
+++ celestial-charm-csp/client/src/App.jsx
@@ -19,6 +19,19 @@ import Quiz from './pages/Dashboard/Personalization/Quiz/Quiz';
 import Results from './pages/Dashboard/Personalization/Results/Results';
 import Wishlist from './pages/Dashboard/Personalization/Wishlist/Wishlist';
 import Books from './pages/Dashboard/Books/Books';
+import Fantasy from './pages/Dashboard/Books/Fantasy/Fantasy';
+import Romance from './pages/Dashboard/Books/Romance/Romance';
+import Mystery from './pages/Dashboard/Books/Mystery/Mystery';
+import Account from './pages/Dashboard/Account/Account';
+import Cart from './pages/Dashboard/Cart/Cart';
+import Fashion from './pages/Dashboard/Shop/Fashion/Fashion';
+import Fragrances from './pages/Dashboard/Shop/Fragrances/Fragrances';
+import Jewelry from './pages/Dashboard/Shop/Jewelry/Jewelry';
+import Policies from './pages/Policies/Policies';
+import PrivacyPolicy from './pages/Policies/PrivacyPolicy/PrivacyPolicy';
+import TermsOfService from './pages/Policies/TermsOfServices/TermsOfServies';
+import About from './pages/About/About';
+import Contact from './pages/Contact/Contact';
 
 function App() {
   
@@ -32,6 +45,20 @@ function App() {
           <Route path="/reviews" element={<Reviews />} />
           <Route path="/register" element={<Register />} />
           <Route path="/login" element={<Login />} />
+          <Route path="/policies" element={<Policies />} />
+          <Route path="/privacy-policy" element={<PrivacyPolicy />} />
+          <Route path="/terms-of-service" element={<TermsOfService />} />
+          <Route path="/about" element={<About />} />
+          <Route path="/contact" element={<Contact />} />
+          {/* Protected Routes */}
+          <Route path="/account" element={<ProtectedRoute><Account /></ProtectedRoute>} />
+          <Route path="/cart" element={<ProtectedRoute><Cart /></ProtectedRoute>} />
+          <Route path="/fashion" element={<ProtectedRoute><Fashion /></ProtectedRoute>} />
+          <Route path="/fragrances" element={<ProtectedRoute><Fragrances /></ProtectedRoute>} />
+          <Route path="/jewelry" element={<ProtectedRoute><Jewelry /></ProtectedRoute>} />
+          <Route path="/fantasy" element={<ProtectedRoute><Fantasy /></ProtectedRoute>} />
+          <Route path="/romance" element={<ProtectedRoute><Romance /></ProtectedRoute>} />
+          <Route path="/mystery" element={<ProtectedRoute><Mystery /></ProtectedRoute>} />
           <Route path="/dashboard" element={<ProtectedRoute><Dashboard /></ProtectedRoute>} />
           <Route path="/shop" element={<ProtectedRoute><Shop /></ProtectedRoute>} />
           <Route path="/kpop2" element={<ProtectedRoute><Kpop2 /></ProtectedRoute>} />
---
+++ celestial-charm-csp/client/src/pages/Dashboard/Books/Romance/Romance.jsx
@@ -0,0 +1,7 @@
+export default function Romance() {
+    return (
+        <>
+        
+        </>
+    );
+};
