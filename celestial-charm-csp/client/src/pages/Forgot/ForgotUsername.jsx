23f9a598314005760fa78c8e50e5afe56bc5ea66
Amanda Hezekiah <amanda.hezekiah@gmail.com>
2025-07-16 18:49:12 -0600

    Update ForgotUsername.jsx

--- celestial-charm-csp/client/src/pages/Forgot/ForgotUsername.jsx
+++ celestial-charm-csp/client/src/pages/Forgot/ForgotUsername.jsx
@@ -2,7 +2,6 @@ import Footer from "../../components/Footer/Footer";
 import NavBar1 from "../../components/NavBars/Navbar1";
 import { useState } from "react";
 import axios from "axios";
-import { redirect } from "react-router-dom";
 
 
 export default function ForgotUsername() {
