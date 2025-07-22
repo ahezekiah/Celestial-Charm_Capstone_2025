6cd04fcba8b128591de6ead4dc6926c83b08c7fb
Amanda Hezekiah <amanda.hezekiah@gmail.com>
2025-07-21 19:42:30 -0600

    quizzes

--- celestial-charm-csp/client/src/pages/Dashboard/Personalization/Quiz/Quiz.jsx
+++ celestial-charm-csp/client/src/pages/Dashboard/Personalization/Quiz/Quiz.jsx
@@ -1,17 +1,29 @@
 import Navbar3 from "../../../../components/NavBars/Navbar3";
 import Footer from "../../../../components/Footer/Footer";
 import { Link } from "react-router-dom";
+import "./Quiz.css";
 
 
 export default function Quiz() {
     return (
         <>
         <Navbar3 />
-        <div className="">
+        <div className="quiz-page">
             <h1>Personalization Quizzes</h1>
             <p>Choose a quiz to get started:</p>
-            <Link to="/quiz/knowledge" className="quiz-link">Knowledge Quiz</Link>
-            <Link to="/quiz/personality" className="quiz-link">Personality Quiz</Link>
+            <div className="quiz-options">
+                <Link to="/quiz/knowledge" className="quiz-link">
+                    <img src="/assets/Wireframe Dither GIF by PERFECTL00P.gif" alt="Knowledge Quiz" className="quiz-image"/>
+                    <h3>Knowledge Quiz</h3>
+                </Link>
+
+                <Link to="/quiz/personality" className="quiz-link">
+                    <img src="/assets/Disney Channel GIF.gif" alt="Personality Quiz"  className="quiz-image"/>
+                    <h3>Personality Quiz</h3>
+                </Link>
+            </div>
+
+            
             <p>These quizzes will help us tailor your experience on Celestial Charm.</p>
         </div>
         <Footer />
