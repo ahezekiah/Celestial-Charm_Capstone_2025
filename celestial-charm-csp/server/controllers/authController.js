f37120a15bd76cf900300f4f426b1a69ba984055
Amanda Hezekiah <amanda.hezekiah@gmail.com>
2025-08-28 23:48:41 -0600

    Update authController.js

--- celestial-charm-csp/server/controllers/authController.js
+++ celestial-charm-csp/server/controllers/authController.js
@@ -1,7 +1,6 @@
 import User from '../models/User.js';
 import bcrypt from 'bcryptjs';
 import jwt from 'jsonwebtoken';
-import User from '../models/User.js';
 const COOKIE_NAME = 'access_token';
 
 function cookieOptions() {
