ce520b4dec3fed5fd86ffc5246a56368a372f461
Amanda Hezekiah <amanda.hezekiah@gmail.com>
2025-09-01 14:53:36 -0600

    Update User.js

--- celestial-charm-csp/server/models/User.js
+++ celestial-charm-csp/server/models/User.js
@@ -12,21 +12,21 @@ const userSchema = new mongoose.Schema({
     profilePicture: { type: String, default: "" },
     gems: { type: Number, default: 0 },
     personalityType: { type: String, default: "" },
-    inventory: { type: [mongoose.Schema.Types.Mixed], default: [{
-        itemId: { type: String },
-        name: { type: String },
-        image: { type: String },
-        priceGems: { type: Number },
-        purchasedAt: { type: Date, default: Date.now }
-    }] },
-
-    // inventory: [{
+    // inventory: { type: [mongoose.Schema.Types.Mixed], default: [{
     //     itemId: { type: String },
     //     name: { type: String },
     //     image: { type: String },
     //     priceGems: { type: Number },
     //     purchasedAt: { type: Date, default: Date.now }
-    // }],
+    // }] },
+
+    inventory: [{
+        itemId: { type: String },
+        name: { type: String },
+        image: { type: String },
+        priceGems: { type: Number },
+        purchasedAt: { type: Date, default: Date.now }
+    }],
 }, { collection: 'users', timestamps: true });
 
 userSchema.methods.comparePassword = async function (plain) {
